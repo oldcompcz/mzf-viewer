ad6fd63ef1c7f0d2c3130dec0131056a7b9c36d8
myrmica-habilis <michalportes1@gmail.com>
2020-09-24 11:47:56 +0200

    Update mock tests

--- test/test_calls.py
+++ test/test_calls.py
@@ -1,45 +1,43 @@
 from unittest.mock import patch, Mock
 
+import pytest
+
 from mzfviewer.__main__ import ViewerApp
 
 
 @patch('mzfviewer.__main__.utils.generate_cgrom')
-def test_generate_cgrom_called_once(generate_cgrom_mock: Mock):
+def test_generate_cgrom_not_called(generate_cgrom_mock: Mock):
     ViewerApp()
-    assert generate_cgrom_mock.called_once()
+    generate_cgrom_mock.assert_not_called()
 
 
-bitmap_dummy = '''#define byte0_width 8
-#define byte0_height 1
-static unsigned char byte0_bits[] = { 0x0 }'''
-
-
-@patch('mzfviewer.__main__.utils.generate_charset',
-       return_value=[bitmap_dummy] * 512)
-def test_generate_charset_call_count_and_args(generate_charset_mock: Mock):
+@patch('mzfviewer.__main__.utils.generate_charset')
+def test_generate_charset_not_called(generate_charset_mock: Mock):
     ViewerApp()
-    assert generate_charset_mock.call_count == 2
-
-    # test values of the `zoom` parameter
-    assert [a.args[1] for a in generate_charset_mock.call_args_list] == [2, 3]
+    generate_charset_mock.assert_not_called()
 
 
-@patch('mzfviewer.__main__.utils.generate_bitmaps',
-       return_value=[bitmap_dummy] * 256)
-def test_generate_bitmaps_call_count_and_args(generate_bitmaps_mock: Mock):
+@pytest.mark.xfail
+@patch('mzfviewer.__main__.utils.generate_asc_to_disp')
+def test_generate_asc_to_disp_not_called(generate_asc_to_disp_mock: Mock):
     ViewerApp()
-    assert generate_bitmaps_mock.call_count == 2
-
-    # test values of the `zoom` parameter
-    assert [a.args[0] for a in generate_bitmaps_mock.call_args_list] == [2, 3]
+    generate_asc_to_disp_mock.assert_not_called()
 
 
-@patch('mzfviewer.__main__.utils.zoomed',
-       side_effect=lambda byte, zoom: b' ' * zoom**2)
-def test_zoomed_call_count_and_args(zoomed_mock: Mock):
+@patch('mzfviewer.__main__.utils.generate_bitmaps')
+def test_generate_bitmaps_not_called(generate_bitmaps_mock: Mock):
     ViewerApp()
-    assert zoomed_mock.call_count == 512*8*2 + 256*2
+    generate_bitmaps_mock.assert_not_called()
 
-    # test all combinations of `byte` and `zoom` parameters
-    assert set(a.args[:2] for a in zoomed_mock.call_args_list) \
-           == set((n, zoom) for n in range(256) for zoom in (2, 3))
+
+@pytest.mark.xfail
+@patch('mzfviewer.__main__.utils.generate_flipped')
+def test_generate_flipped_not_called(generate_flipped_mock: Mock):
+    ViewerApp()
+    generate_flipped_mock.assert_not_called()
+
+
+@patch('mzfviewer.__main__.utils.zoomed')
+def test_zoomed_not_called(zoomed_mock: Mock):
+    ViewerApp()
+    zoomed_mock.assert_not_called()
